200e726bce80296451b2bb13dfd791a272be2aab
Alex173 <sashakechedzhy@gmail.com>
2022-09-14 20:32:17 +0300

    Add MainViewModel class

---
+++ WeatherTestTask/Flow/MainScreen/MainViewModel.swift
@@ -0,0 +1,20 @@
+//
+//  MainViewModel.swift
+//  WeatherTestTask
+//
+//  Created by Alex173 on 14.09.2022.
+//
+
+import Foundation
+
+protocol MainViewModelInterface {
+    
+}
+
+class MainViewModel: NSObject, MainViewModelInterface {
+    
+    weak var coordinator: MainCoordinatorInteface?
+    
+    
+    
+}
