8c8372a34920cebc6db04b316359db1b74b7c0b9
Alex173 <sashakechedzhy@gmail.com>
2022-09-17 11:20:42 +0300

    Add IconButton class

---
+++ WeatherTestTask/Views/IconButton.swift
@@ -0,0 +1,38 @@
+//
+//  IconButton.swift
+//  WeatherTestTask
+//
+//  Created by Alex173 on 17.09.2022.
+//
+
+import UIKit
+import SnapKit
+
+class IconButton: UIButton {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+
+    init(icon: UIImage?) {
+        super.init(frame: .zero)
+        let newImage = icon?.withRenderingMode(.alwaysTemplate)
+        setImage(newImage, for: .normal)
+        tintColor = R.color.white()
+        snp.makeConstraints {
+            $0.width.height.equalTo(32)
+        }
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override var isHighlighted: Bool {
+        didSet {
+            alpha = isHighlighted ? 0.5 : 1.0
+        }
+    }
+    
+}
+
