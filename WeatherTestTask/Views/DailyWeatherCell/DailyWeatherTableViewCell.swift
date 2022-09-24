9546e76be5d64b53c95b171b3fbd78442bb1cb76
Alex173 <sashakechedzhy@gmail.com>
2022-09-24 20:33:44 +0300

    Add DailyWeatherCell & HourlyWeatherCell classes

---
+++ WeatherTestTask/Views/DailyWeatherCell/DailyWeatherTableViewCell.swift
@@ -0,0 +1,8 @@
+//
+//  DailyWeatherCell.swift
+//  WeatherTestTask
+//
+//  Created by Alex173 on 20.09.2022.
+//
+
+import Foundation
---
+++ WeatherTestTask/Views/HourlyWeatherCell/HourlyWeatherCollectionViewCell.swift
@@ -0,0 +1,96 @@
+//
+//  HourlyWeatherCollectionViewCell.swift
+//  WeatherTestTask
+//
+//  Created by Alex173 on 20.09.2022.
+//
+
+import UIKit
+
+class HourlyWeatherCollectionViewCell: UICollectionViewCell {
+    
+    private let hourLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = R.color.white()
+        label.font = UIFont.systemFont(ofSize: 22)
+        label.text = "17"
+        return label
+    }()
+    
+    private let minuteLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = R.color.white()
+        label.font = UIFont.systemFont(ofSize: 14)
+        label.text = "00"
+        return label
+    }()
+    
+    private let weatherImageView: UIImageView = {
+        let image = R.image.icon_white_day_cloudy()?.withRenderingMode(.alwaysTemplate)
+        let imageView = UIImageView(image: image)
+        imageView.tintColor = R.color.white()
+        imageView.contentMode = .scaleAspectFit
+        return imageView
+    }()
+    
+    private let temperatureLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = R.color.white()
+        label.font = UIFont.systemFont(ofSize: 22)
+        label.text = "27°"
+        return label
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configureCell()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configureCell() {
+        backgroundColor = .clear
+        configureHourLabel()
+        configureMinuteLabel()
+        configureWeatherImageView()
+        configureTemperatureLabel()
+    }
+    
+    private func configureHourLabel() {
+        addSubview(hourLabel)
+        hourLabel.snp.makeConstraints {
+            $0.top.equalToSuperview().inset(16)
+            $0.leading.equalToSuperview().inset(20)
+        }
+    }
+    
+    private func configureMinuteLabel() {
+        addSubview(minuteLabel)
+        minuteLabel.snp.makeConstraints {
+            $0.top.equalToSuperview().inset(18)
+            $0.leading.equalTo(hourLabel.snp.trailing)
+        }
+    }
+    
+    private func configureWeatherImageView() {
+        addSubview(weatherImageView)
+        weatherImageView.snp.makeConstraints {
+            $0.top.equalTo(hourLabel.snp.bottom).inset(-16)
+            $0.centerX.equalToSuperview()
+            $0.width.height.equalTo(40)            
+        }
+    }
+    
+    private func configureTemperatureLabel() {
+        addSubview(temperatureLabel)
+        temperatureLabel.snp.makeConstraints {
+            $0.top.equalTo(weatherImageView.snp.bottom)
+            $0.centerX.equalToSuperview()
+            
+        }
+    }
+
+    
+}
